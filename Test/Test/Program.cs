5b04426a19c058f174c0eb8dc659f3609ff716f2
hmsamit <amithzlmae@gmail.com>
2021-06-30 20:01:05 +0800

    test

---
+++ Test/Test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            Console.Write("Click number 1 or 2\n");
+            string choice = Console.ReadLine();
+
+            if (choice == "1")
+            {
+                Console.WriteLine("Hello World!");
+            }
+            else if (choice == "2")
+            {
+                Console.WriteLine("Hello Saturn!");
+            }
+            else
+            {
+                Console.WriteLine("ERROR"); 
+            }
+            Console.ReadKey();
+        }
+    }
+}
